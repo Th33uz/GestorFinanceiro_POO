1f010ec5726c13a04e96eeaa97b30332cf2b72b0
Gabriel-EngSoftware <gabrielbarbosadossantos19@gmail.com>
2025-04-06 15:28:16 -0300

    Feat - Add Interface Principal

---
+++ GestorFinanceiro/Interface_Principal.java
@@ -0,0 +1,72 @@
+package GestorFinanceiro;
+
+import javax.swing.*;
+
+public class Interface_Principal extends JFrame{
+    private JButton BotaoCategorias, BotaoTransacoes, BotaoHistorico, BotaoResumo, BotaoVoltar;
+    private Estrutura_Usuario usuario;
+    private Controle_Usuario controleUsuario;
+
+    public Interface_Principal(Estrutura_Usuario usuario, Controle_Usuario controleUsuario){
+        this.usuario = usuario;
+        this.controleUsuario = controleUsuario;
+
+        setTitle("Tela Inicial");
+        setSize(420, 330);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setLayout(null);
+
+        JLabel RotuloMensagem = new JLabel("Bem-vindo(a) - " + usuario.getNome());
+        RotuloMensagem.setBounds(150, 20, 200, 25);
+        add(RotuloMensagem);
+
+        BotaoCategorias = new JButton("Categorias");
+        BotaoCategorias.setBounds(150, 60, 120, 25);
+        add(BotaoCategorias);
+
+        BotaoTransacoes = new JButton("Transações");
+        BotaoTransacoes.setBounds(150, 100, 120, 25);
+        add(BotaoTransacoes);
+
+        BotaoHistorico = new JButton("Histórico");
+        BotaoHistorico.setBounds(150, 140, 120, 25);
+        add(BotaoHistorico);
+
+        BotaoResumo = new JButton("Resumo");
+        BotaoResumo.setBounds(150, 180, 120, 25);
+        add(BotaoResumo);
+
+        BotaoVoltar = new JButton("Voltar");
+        BotaoVoltar.setBounds(150, 220, 120, 25);
+        add(BotaoVoltar);
+
+        BotaoCategorias.addActionListener(e -> {
+            //adicionar o construtor da classe depois para inicializar o frame da proxima tela
+            //não esquecer de colocar o dispose para fechar a tela principal
+        });
+
+        BotaoTransacoes.addActionListener(e -> {
+            //adicionar o construtor da classe depois para inicializar o frame da proxima tela
+            //não esquecer de colocar o dispose para fechar a tela principal
+        });
+
+        BotaoHistorico.addActionListener(e -> {
+            //adicionar o construtor da classe depois para inicializar o frame da proxima tela
+            //não esquecer de colocar o dispose para fechar a tela principal
+        });
+
+        BotaoResumo.addActionListener(e -> {
+            //adicionar o construtor da classe depois para inicializar o frame da proxima tela
+            //não esquecer de colocar o dispose para fechar a tela principal
+        });
+
+        BotaoVoltar.addActionListener(e -> {
+            //adicionar o construtor da classe depois para inicializar o frame da proxima tela
+            //não esquecer de colocar o dispose para fechar a tela principal
+            //o construtor do botão voltar é o da tela de login!!!
+        });
+
+        setVisible(true);
+    }
+}
